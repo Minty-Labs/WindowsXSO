154c847132a5a60fc7adcca5e05d1ee809c28983
Lily <admin@mintlily.lgbt>
2023-10-29 09:54:17 -0400

    [Config] Added Dev Vars
    partially for auto update

--- WindowsXSO/Configuration.cs
+++ WindowsXSO/Configuration.cs
@@ -1,36 +1,50 @@
-using System.Text.Json;
+﻿using System.Text.Json;
 using System.Text.Json.Serialization;
 using Serilog;
 
 namespace WindowsXSO; 
 
 public class Configuration {
+    [JsonPropertyName("Developer Variables (Please never change these values, unless told otherwise for debugging)")] public DeveloperVars DeveloperVars { get; set; }
     [JsonPropertyName("Toggle Comment")] public string? ToggleComment { get; set; }
     [JsonPropertyName("Enable Whitelist")] public bool EnableWhitelist { get; set; }
     [JsonPropertyName("Target Application Names")] public List<string>? TargetApplicationNames { get; set; }
     [JsonPropertyName("Auto-Close with SteamVR")] public bool AutoCloseWithSteamVr { get; set; }
+    [JsonPropertyName("Auto Update")] public bool AutoUpdate { get; set; }
+}
+
+public class DeveloperVars {
+    [JsonPropertyName("Config Version")] public int ConfigVersion { get; set; }
+    [JsonPropertyName("Was it updated?")] public bool WasItUpdated { get; set; }
+    [JsonPropertyName("Use Shell Execute")] public bool UseShellExecute { get; set; }
 }
 
 public static class Config {
     public static Configuration? Configuration { get; private set; }
-    private static DirectoryInfo ConfigDirectory { get; set; } = new (Environment.CurrentDirectory);
     
     public static void LoadConfig() {
-        if (!File.Exists(ConfigDirectory + "/Config.json")) {
+        if (!File.Exists(Path.Combine(Environment.CurrentDirectory, "Config.json"))) {
             var defaultConfig = new Configuration {
                 ToggleComment = "True - Enable Whitelist (Only allow specified), False - Enable Blacklist (Block specified)",
                 EnableWhitelist = true,
                 TargetApplicationNames = new List<string> { "discord", "discord ptb", "discord canary" },
                 AutoCloseWithSteamVr = true,
+                AutoUpdate = true,
+                DeveloperVars = new DeveloperVars {
+                    ConfigVersion = 2,
+                    WasItUpdated = false,
+                    UseShellExecute = false
+                }
             };
             File.WriteAllText(Path.Combine(Environment.CurrentDirectory, "Config.json"), JsonSerializer.Serialize(defaultConfig, new JsonSerializerOptions { WriteIndented = true }));
             Log.Information("[{0}] Created Config.json", "CONFIG");
             Configuration = defaultConfig;
             return;
         }
-        Configuration = JsonSerializer.Deserialize<Configuration>(File.ReadAllText(ConfigDirectory + "/Config.json"))!;
-        Log.Information("[{0}] Loaded Config.json", "JSON");
+        Configuration = JsonSerializer.Deserialize<Configuration>(File.ReadAllText(Path.Combine(Environment.CurrentDirectory, "Config.json")))!;
+        Log.Information("[{0}] Loaded from Config.json", "CONFIG");
+        Save();
     }
     
-    public static void Save() => File.WriteAllText(ConfigDirectory + "/Config.json", JsonSerializer.Serialize(Configuration, new JsonSerializerOptions { WriteIndented = true }));
+    public static void Save() => File.WriteAllText(Path.Combine(Environment.CurrentDirectory, "Config.json"), JsonSerializer.Serialize(Configuration, new JsonSerializerOptions { WriteIndented = true }));
 }
