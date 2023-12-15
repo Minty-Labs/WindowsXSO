7e9ea5fac60e0e933ed9889ff35f53e47564f866
Lily <admin@mintlily.lgbt>
2023-12-14 19:36:31 -0500

    Added Window Minimizer

---
+++ WindowsXSO/Minimizer.cs
@@ -0,0 +1,18 @@
+﻿using System.Runtime.InteropServices;
+
+namespace WindowsXSO;
+
+public static class Minimizer {
+    [DllImport("user32.dll")]
+    [return: MarshalAs(UnmanagedType.Bool)]
+    private static extern bool ShowWindow(IntPtr hWnd, int nCmdShow);
+    
+    [DllImport("kernel32.dll")]
+    private static extern IntPtr GetConsoleWindow();
+    
+    public static async void Minimize() {
+        await Task.Delay(TimeSpan.FromSeconds(10));
+        var handle = GetConsoleWindow();
+        ShowWindow(handle, 0);
+    }
+}
